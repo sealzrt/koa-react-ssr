93e4b5674c37323c95324d63cea5c28784513a21
张瑞涛 <zhangruitao@mmears.com>
2020-04-15 17:46:21 +0800

    update

--- packages/ch05-my-react-ssr-dataiso/src/server/middlewares/react-ssr.js
+++ packages/ch05-my-react-ssr-dataiso/src/server/middlewares/react-ssr.js
@@ -3,12 +3,10 @@
 //完成 react ssr 工作的中间件
 //引入Index 组件
 import React from 'react';
-import { renderToString } from 'react-dom/server';
+import {renderToString} from 'react-dom/server';
 
-import { StaticRouter, Route, matchPath} from 'react-router';
-import { renderRoutes} from 'react-router-config';
+import {StaticRouter} from 'react-router';
 
-import Layout from '../../client/app/layout';//如果有 layout 组件，也需要一起转换为 html
 import routeList from '../../client/router/route-config';
 
 //自定义 provider 用来传递数据
@@ -18,38 +16,38 @@ import matchRoute from '../../share/match-route';
 
 import App from '../../client/router/index';
 
-export default  async (ctx,next)=>{
+export default async (ctx, next) => {
 
-    const path = ctx.request.path;
+  const path = ctx.request.path;
 
-    if(path.indexOf('.')>-1){
-        ctx.body=null;
-        return next();
-    }
+  if (path.indexOf('.') > -1) {
+    ctx.body = null;
+    return next();
+  }
 
-    console.log('ctx.request.path.', ctx.request.path);
+  console.log('ctx.request.path.', ctx.request.path);
 
-    //查找到的目标路由对象
-    let matchResult = matchRoute(path,routeList);
-    let { targetRoute, targetMatch }=matchResult;
+  //查找到的目标路由对象
+  const {targetRoute} = matchRoute(path, routeList);
 
-    //得到数据
-    let fetchDataFn = targetRoute.component.getInitialProps;
-    let fetchResult = {};
-    if(fetchDataFn){
-        fetchResult = await fetchDataFn();
-    }
+  //得到数据
+  let fetchDataFn = targetRoute.component.getInitialProps;
+  let fetchResult = {};
+  if (fetchDataFn) {
+    fetchResult = await fetchDataFn();
+  }
 
-    //将预取数据在这里传递过去 组内通过props.staticContext获取
-    const context = {
-        initialData: fetchResult
-    };
+  //将预取数据在这里传递过去 组内通过props.staticContext获取
+  const context = {
+    initialData: fetchResult
+  };
 
-    const html = renderToString(<StaticRouter location={path} context={context}>
-        <App routeList={routeList}></App>
+  const html = renderToString(
+    <StaticRouter location={path} context={context}>
+      <App routeList={routeList}></App>
     </StaticRouter>);
 
-    ctx.body=`<!DOCTYPE html>
+  ctx.body = `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -65,5 +63,5 @@ export default  async (ctx,next)=>{
 </body>
 </html><script type="text/javascript"  src="/index.js"></script>`;
 
-    await next();
+  await next();
 }
